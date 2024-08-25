952537b21d24407bf3cc6ac979b0be2438adc95d
Ngo Jun Hao Jason <njhjason@protonmail.com>
2024-08-25 16:13:33 +1200

    Add landing page banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import TopBar from "./ui/TopBar";
 import styles from "./page.module.css";
+import Banner from "./ui/Banner";
 
 const LandingPage = () => {
   return (
     <>
       <TopBar />
+      <Banner />
     </>
   );
 };
