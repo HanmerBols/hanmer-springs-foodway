cfa7b27459befe3864b7f215f7f0b80e96119e7e
Ngo Jun Hao Jason <njhjason@protonmail.com>
2024-09-05 22:51:43 +1200

    Update Noodles tagline

--- app/noodles/content.ts
+++ app/noodles/content.ts
@@ -1,5 +1,5 @@
 export const NOODLES_PAGE_TITLE = "Noodles";
-export const NOODLES_PAGE_SUBTITLE = "Some catchy tagline for Noodles";
+export const NOODLES_PAGE_SUBTITLE = "Oodles of tastiness";
 
 export const NOODLES_IMAGE_PATHS = [
   "/sub_pages/noodles/noodles_1.png",
