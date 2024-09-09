31e81d025c76dbc62c073a9727b021adfb60ab94
Hanmer Springs Foodway <hanmerbols@gmail.com>
2024-09-09 13:13:01 +1200

    4th image in noodle subpage added

--- app/noodles/content.ts
+++ app/noodles/content.ts
@@ -5,4 +5,5 @@ export const NOODLES_IMAGE_PATHS = [
   "/sub_pages/noodles/noodles_1.png",
   "/sub_pages/noodles/noodles_2.png",
   "/sub_pages/noodles/noodles_3.png",
+  "/sub_pages/noodles/noodles_4.png",
 ];
